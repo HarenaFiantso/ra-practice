da7a63fdc28166c970f7fa10b6d98bac8b2cd7d9
HarenaFiantso <fiantsorav@gmail.com>
2024-04-28 11:57:29 +0300

    feat: comment page implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Admin, Resource } from 'react-admin';
 import { dataProvider } from './providers/dataProvider';
-import post from './operations';
+import post from './operations/post';
+import comments from "./operations/comments";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name='posts' {...post} />
+    <Resource name='comments' {...comments} />
   </Admin>
 );
---
+++ src/operations/comments/CommentList.tsx
@@ -0,0 +1,5 @@
+import {ReactNode} from "react";
+
+export default function CommentList(): ReactNode {
+    return <>Comment List</>
+}
